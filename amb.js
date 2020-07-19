27322e04844f8cedcc13c6995ffafa8105e73a86
maurya20 <mukesh.ice17@gmail.com>
2020-07-19 10:23:11 +0530

    Live day and time implemented on pages sucessfully.

--- amb.js
+++ amb.js
@@ -20,3 +20,22 @@ function myFunction() {
   alert("Form submitted sucessfully1111!");
 }
 
+function myClk() {
+  d = new Date();
+  day = d.get
+  m = d.getSeconds();
+  z = d.getHours();
+  y = d.getMinutes();
+  x = m%100;
+  weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  n = weekday[d.getDay()];
+  document.getElementById("time").innerHTML = n+":   "+z+":"+y +":"+ x;
+}
